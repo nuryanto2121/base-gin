809ad0850799e4c6402186cb0d47a056395ce078
arsyila105 <andrianrizky356@gmail.com>
2022-08-03 21:20:57 +0700

    update skumanagement

--- models/m_sku_management.go
+++ models/m_sku_management.go
@@ -5,8 +5,8 @@ import uuid "github.com/satori/go.uuid"
 type SkuManagement struct {
 	Id           uuid.UUID `json:"id" gorm:"primary_key;type:uuid;DEFAULT:uuid_generate_v4()"`
 	SkuName      string    `json:"sku_name" gorm:"type:varchar(60);Index:idx_skuname,unique;not null"`
-	Duration     int       `json:"duration" gorm:"type:int(64)"`
-	PriceWeekDay int       `json:"price_weekday" gorm:"type:numeric(20,2)"`
-	PriceWeekEnd int       `json:"price_weekend" gorm:"type:numeric(20,2)"`
+	Duration     int64     `json:"duration" gorm:"type:integer"`
+	PriceWeekDay int64     `json:"price_weekday" gorm:"type:numeric(20,2)"`
+	PriceWeekEnd int64     `json:"price_weekend" gorm:"type:numeric(20,2)"`
 	Model
 }
